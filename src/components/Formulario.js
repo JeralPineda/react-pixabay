ebdcfaf3505c9de397b14728b535791e5f1ce4f4
Jeral <jeral.cris@gmail.com>
2021-12-27 11:06:33 -0600

    validacion del formulario y componente de error

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,25 @@
+import { useState } from 'react';
+
+import Error from './Error';
+
 const Formulario = () => {
+   const [termino, setTermino] = useState('');
+   const [error, setError] = useState(false);
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+
+      // Validar el formulario
+      if (termino.trim() === '') {
+         setError(true);
+         return;
+      }
+
+      setError(false);
+   };
+
    return (
-      <form>
+      <form onSubmit={handleSubmit}>
          <div className='row'>
             <div className='form-group col-md-8'>
                <input
@@ -8,6 +27,7 @@ const Formulario = () => {
                   type='text'
                   className='form-control form-control-lg'
                   placeholder='Busca una imagen'
+                  onChange={(e) => setTermino(e.target.value)}
                />
             </div>
 
@@ -21,6 +41,8 @@ const Formulario = () => {
                </button>
             </div>
          </div>
+
+         {error ? <Error mensaje='Agrega un termino de búsqueda' /> : null}
       </form>
    );
 };
